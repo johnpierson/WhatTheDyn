74b6bec2b5237da4132c84447a5bab42619ae516
John <sixtysecondrevit@gmail.com>
2024-10-01 19:56:10 -0600

    Resources again

--- source/WhatTheDyn/App.cs
+++ source/WhatTheDyn/App.cs
@@ -111,7 +111,7 @@ namespace WhatTheDynamo
             //build our notification bubble
             ResultItem result = new ResultItem
             {
-                Title = $"{Properties.Resources.ResourceManager.GetString("NotificationBubble")} {Global.DynamoVersion}",
+                Title = $"{WhatTheDyn.Properties.Resources.ResourceManager.GetString("NotificationBubble")} {Global.DynamoVersion}",
                 Category = "What the Dyn?!",
                 IsNew = true,
                 Timestamp = DateTime.Now
